92bcfc9b64454f8627d218e4205955924f03ef5c
zmj <zhouminjun@kezaihui.com>
2018-07-14 14:50:35 +0800

    bot 0.1

---
+++ image.py
@@ -0,0 +1,26 @@
+from PIL import Image, ImageDraw, ImageFont
+
+font_file = 'simhei.ttf'
+
+
+def ps(text, name, high_size=700, font_size=80, color=(255, 255, 255)):
+    img = Image.open('{}.jpg'.format(name))  # type: Image.Image
+    if name == '1':
+        font_size = 24
+        high_size = 120
+        color = (0, 0, 0)
+    elif name == '2':
+        font_size = 45
+        high_size = 350
+        color = (0, 0, 0)
+    img_width = img.width  # 194
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype(font_file, size=font_size)
+    left = max((img_width - len(text) * font_size) // 2, 0)
+    draw.text((left, high_size), text, color, font=font)
+    img.save('002.jpg')
+
+
+if __name__ == '__main__':
+    ps('我我我我我我我我', '2')
+
---
+++ wechat.py
@@ -0,0 +1,93 @@
+import random
+import os
+import tempfile
+
+from wxpy import *
+from weather import *
+from image import *
+
+
+# cache_path = True
+bot = Bot(console_qr=True, cache_path=True)
+
+# bot.enable_puid('wxpy_puid.pk1')
+
+alias = []
+
+group = bot.groups(update=True).search('皈依佛门')[0]  # 群名
+
+group1 = bot.groups(update=True).search('几时')[0]
+
+print(group1)
+print(group)
+friend = bot.search('强哥')[0]  # 好友昵称
+friend1 = bot.search('柔弱')[0]
+
+@bot.register(friend1, except_self=False)
+def print_qiang(msg):
+    if msg.text[-2:] == '天气':
+        weather = get_weather(msg.text[:-2])
+        msg.reply(weather)
+    if '是不是' in msg.text:
+        msg.reply(random.choice(['是', '不是']))
+    if msg.text[:1] == 'p':
+        text = msg.text.split(' ')[1]
+        # print(text)
+        if not text:
+            return 'p <text>'
+        ps(text)
+        msg.reply_image('002.jpg')
+
+
+# 可自行添加alias
+@bot.register(group, except_self=False)
+def print_group(msg):
+    if msg.text[:1] == 'p':
+        text = '\n'.join(msg.text.split(' ')[1:])
+        # print(text)
+        if not text:
+            return 'p <text>'
+        ps(text, msg.text.split(' ')[0][1:])
+        msg.reply_image('002.jpg')
+    else:
+        if '是不是' in msg.text:
+            msg.reply(random.choice(['是', '不是']))
+        if '有没有' in msg.text:
+            msg.reply(random.choice(['有', '没有，滚']))
+        if '吴越' in msg.text:
+            ans = ['我们的儿子', '吴越大帅哥', '吴越瓜皮', '越秀白敬亭', '泰顺大张伟', '大厨', '有妇之夫']
+            msg.reply(random.choice(ans))
+        for i in ['lt', '凉亭', '梁挺']:
+            if i in msg.text:
+                ans = ['渣男', '禽兽', '吴越儿子']
+                msg.reply(random.choice(ans))
+        if '星星' in msg.text:
+            ans = ['曾经的胡歌', '如今的林更新', '星哥好帅', '吴越的父亲', '也是一个瓜皮']
+            msg.reply(random.choice(ans))
+        if '小马林' in msg.text:
+            ans = ['小马林C位出道', '土味老哥', '越秀男神', '火影转世', '居居男孩']
+            msg.reply(random.choice(ans))
+        if msg.text[-2:] == '天气':
+            weather = get_weather(msg.text[:-2])
+            msg.reply(weather)
+
+
+@bot.register(bot.self, except_self=False)
+def print_other(msg):
+    if '随机' in msg.text:
+        ans = random.randint(0, 100)
+        msg.reply(ans)
+    if msg.text[-2:] == '天气':
+        weather = get_weather(msg.text[:-2])
+        msg.reply(weather)
+    if '是不是' in msg.text:
+        msg.reply(random.choice(['是', '不是']))
+    if msg.text[:2] == 'ps':
+        text = msg.text.split(' ')[1]
+        if not text:
+            return 'ps <text>'
+        ps(text)
+        msg.reply_image('002.jpg')
+
+
+embed()
