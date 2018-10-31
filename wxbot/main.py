aa16eec4a970238a4f478af7b71fb989929a2202
zhouminjun <zhouminjun@kezaihui.com>
2018-10-31 20:27:53 +0800

    feat: add flake8

--- wxbot/main.py
+++ wxbot/main.py
@@ -1,14 +1,19 @@
 import itchat
-
-itchat.auto_login()
-
-itchat.send('Hello World', toUserName='filehelper')
+from itchat.content import MAP, TEXT
 
 
-@itchat.msg_register(itchat.content.TEXT)
+# TODO 常量单独放个文件，敏感信息存环境变量
+friend = itchat.search_friends('xxx')
+print(friend)
+
+
+@itchat.msg_register(msgType=[TEXT, MAP], isFriendChat=True)  # 消息类型
 def text_reply(msg):
-    return msg.text
+    print(msg.user)
+    if msg.user['RemarkName'] in ['陈亮']:
+        return msg.user.send('I am robot')
 
 
 if __name__ == '__main__':
+    itchat.auto_login(enableCmdQR=2, hotReload=True)
     itchat.run()
