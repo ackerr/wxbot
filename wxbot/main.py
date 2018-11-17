1afd51842c8126c0183f4d7a7119b27f76641a5d
zhouminjun <zhouminjun@kezaihui.com>
2018-11-17 20:53:02 +0800

    feat: add weather api

--- wxbot/main.py
+++ wxbot/main.py
@@ -1,19 +1,57 @@
 import itchat
-from itchat.content import MAP, TEXT
+from itchat.content import MAP, PICTURE, TEXT, VIDEO
+from wxbot.weather import get_weather
+
+itchat.auto_login(enableCmdQR=2, hotReload=True)
 
 
-# TODO 常量单独放个文件，敏感信息存环境变量
-friend = itchat.search_friends('xxx')
-print(friend)
+def get_group(nickname, goal_group=None):
+    groups = itchat.get_chatrooms(update=True)
+    for group in groups:
+        if group['NickName'] == nickname:
+            goal_group = group['UserName']
+            print(group)
+    return goal_group
 
 
-@itchat.msg_register(msgType=[TEXT, MAP], isFriendChat=True)  # 消息类型
-def text_reply(msg):
-    print(msg.user)
-    if msg.user['RemarkName'] in ['陈亮']:
-        return msg.user.send('I am robot')
+def get_friend(nickname, goal_friend=None):
+    friends = itchat.get_friends(update=True)
+    for friend in friends:
+        if friend['NickName'] == nickname:
+            goal_friend = friend['UserName']
+    return goal_friend
 
 
-if __name__ == '__main__':
-    itchat.auto_login(enableCmdQR=2, hotReload=True)
-    itchat.run()
+@itchat.msg_register([TEXT, MAP], isGroupChat=True)
+def send_weather(msg):
+    if msg['ToUserName'] == get_group('小吴全球粉丝后援会'):
+        if msg.Content[-2:] == '天气':
+            weather = get_weather(msg.Content[:-2])
+            itchat.send(weather, toUserName=msg['ToUserName'])
+
+    if msg['ToUserName'] == get_group('30天 Django 实战训练营'):
+        if msg.Content[-2:] == '天气':
+            weather = get_weather(msg.Content[:-2])
+            itchat.send(weather, toUserName=msg['ToUserName'])
+
+
+@itchat.msg_register([TEXT, MAP])
+def text_func(msg):
+    if msg['ToUserName'] == get_friend('ZmJ'):
+        print(msg.Content)
+        if msg.Content[-2:] == '天气':
+            weather = get_weather(msg.Content[:-2])
+            itchat.send(weather, toUserName=msg['ToUserName'])
+
+
+@itchat.msg_register([PICTURE, VIDEO])
+def download_files(msg):
+    """ 下载视频 """
+    if msg['ToUserName'] == get_group('小吴全球粉丝后援会'):
+        msg.download(msg.fileName)
+    # 回复图片
+    # itchat.send('@{}@{}'.format('img' if msg['Type'] == 'Picture' else 'fil', msg['FileName']), msg['FromUserName'])
+    # return f'{msg["Type"]} received'
+
+
+itchat.run(debug=True)  # 一直跑
---
+++ wxbot/weather.py
@@ -0,0 +1,42 @@
+import os
+
+import requests
+
+
+k = os.environ.get('KEY')
+
+
+def get_city_number(city_name):
+    r = requests.get(
+        'http://restapi.amap.com/v3/config/district?key={}&keywords={}&subdistrict={}'.format(k, city_name, 0))
+    if r.json()['count'] == '0':
+        adcode = '330109'
+    else:
+        adcode = r.json()['districts'][0]['adcode']
+    return adcode
+
+
+def get_weather(city_name, key=k):
+    if not city_name:
+        city_name = '萧山'
+
+    city_num = get_city_number(city_name)
+    if city_name == '澳门':
+        city_num = '820000'
+    r = requests.get(
+        'http://restapi.amap.com/v3/weather/weatherInfo?key={}&city={}&extensions={}'.format(key, city_num, 'all'))
+    fore = r.json()['forecasts'][0]
+    city = fore['city']
+    time = fore['reporttime'].split(' ')[0]
+    for i in fore['casts']:
+        if i['date'] == time:
+            day_weather = i['dayweather']
+            night_weather = i['nightweather']
+            day_temp = i['daytemp']
+            night_temp = i['nighttemp']
+            return '{} \n白天:{}  温度{}\n晚上:{}  温度{}'.format(city, day_weather, day_temp, night_weather, night_temp)
+
+
+if __name__ == '__main__':
+    print(get_city_number(''))
+    print(get_weather('狗屎'))
